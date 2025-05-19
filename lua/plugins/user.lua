4729374533ba70daef70f4255a759cccaa9c1efc
wKoja <cfwilliam.ferreira@gmail.com>
2025-05-19 15:37:07 -0300

    clean up

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -15,47 +15,6 @@ return {
 
   -- == Examples of Overriding Plugins ==
 
-  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-    end,
-  },
-
-  {
-    "windwp/nvim-autopairs",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom autopairs configuration such as custom rules
-      local npairs = require "nvim-autopairs"
-      local Rule = require "nvim-autopairs.rule"
-      local cond = require "nvim-autopairs.conds"
-      npairs.add_rules(
-        {
-          Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
-        },
-        -- disable for .vim files, but it work for another filetypes
-        Rule("a", "a", "-vim")
-      )
-    end,
-  },
-
   {
     "nvim-neotest/neotest",
     dependencies = {
@@ -76,21 +35,12 @@ return {
       }
     end,
   },
-  {
-    "rcarriga/nvim-dap-ui",
-    version = false,
-    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-  },
-
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-  },
 
   {
     "tpope/vim-surround",
     lazy = false,
   },
+
   {
     "tpope/vim-fugitive",
     lazy = false,
@@ -117,67 +67,6 @@ return {
     lazy = false,
   },
 
-  {
-    "mrjones2014/smart-splits.nvim",
-    lazy = false,
-    config = function()
-      require("smart-splits").setup {
-        -- Ignored buffer types (only while resizing)
-        ignored_buftypes = {
-          "nofile",
-          "quickfix",
-          "prompt",
-        },
-        ignored_filetypes = { "NvimTree" },
-        default_amount = 3,
-        at_edge = "wrap",
-        float_win_behavior = "previous",
-        move_cursor_same_row = false,
-        cursor_follows_swapped_bufs = false,
-        resize_mode = {
-          -- key to exit persistent resize mode
-          quit_key = "<ESC>",
-          -- keys to use for moving in resize mode
-          -- in order of left, down, up' right
-          resize_keys = { "h", "j", "k", "l" },
-          -- set to true to silence the notifications
-          -- when entering/exiting persistent resize mode
-          silent = false,
-          -- must be functions, they will be executed when
-          -- entering or exiting the resize mode
-          hooks = {
-            on_enter = nil,
-            on_leave = nil,
-          },
-        },
-        ignored_events = {
-          "BufEnter",
-          "WinEnter",
-        },
-        log_level = "info",
-      }
-      -- recommended mappings
-      -- resizing splits
-      -- these keymaps will also accept a range,
-      -- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
-      vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
-      vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
-      vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
-      vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
-      -- moving between splits
-      vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
-      vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
-      vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
-      vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
-      vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
-      -- swapping buffers between windows
-      vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
-      vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
-      vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
-      vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
-    end,
-  },
-
   -- lazy stuff
 
   { "nvim-pack/nvim-spectre" },
@@ -194,16 +83,6 @@ return {
         background = "#1E1E2E",
         to_clipboard = true,
       }
-    end,
-  },
-
-  {
-    "monkoose/neocodeium",
-    event = "VeryLazy",
-    config = function()
-      local neocodeium = require "neocodeium"
-      neocodeium.setup()
-      vim.keymap.set("i", "<A-f>", neocodeium.accept)
     end,
   },
 
